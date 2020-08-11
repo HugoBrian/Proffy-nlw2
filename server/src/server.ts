0d436f074c6960ece81f9abf1174e47296ab17d1
HugoBrian <hugo_brian@hotmail.com>
2020-08-11 16:21:55 -0300

    Criação do banco de dados SQLite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,34 @@
 import express from 'express';
+import routes from './routes'
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
+
+// MÉTODOS HTTP
 //GET: Buscar ou listar uma informação
 //POST: Criar alguma nova informação no Back-end
 //PUT: Atualizar uma informação já existente
 //DELETE: Deletar uma informação existente
 
-app.post('/users', (request, response) => {
-    const users = [
-        { name: 'Diego', age: 25},
-        { name: 'Vini', age: 21},
-    ]
-    return response.json(users);
-});
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+//      app.get('/users', (request, response) => {
+//      console.log(request.body);
+// insomnia: http://localhost:3333/users
 
-app.listen(3333);
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+//      app.delete('/users:id', (request, response) => {
+//      console.log(request.params);
+// insomnia: http://localhost:3333/users/1
+
+// Query Params: Paginação, filtros, ordenação
+//      app.get('/users', (request, response) => {
+//      console.log(request.query);
+// insomnia: http://localhost:3333/users?page=2&sort=name&
+
+
+
